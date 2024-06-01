6457e71a538ca10656ab740983fdec7d74b30e73
Siddhesh Kumar <siddheshkr21@gmail.com>
2024-06-01 21:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (42.17%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int ans = 0;
-        for (int i = 1; i < s.size(); i++) {
-            ans += abs(s[i - 1] - s[i]);
-        }
-        return ans;
+        int n = s.size(), res = 0;
+        for(int i=0;i<n-1;i++)res+=abs(s[i]-s[i+1]);
+        return res;
     }
 };
