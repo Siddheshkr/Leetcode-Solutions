810cc91d136b4cfb6f332ed35ce1b5d8ea85585e
Siddhesh Kumar <siddheshkr21@gmail.com>
2024-10-02 14:42:31 +0530

    Time: 117 ms (12.33%) | Memory: 44 MB (43.30%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map <int, int> mp;
-        for(auto &it: arr)
-            mp[it] = 0;
-        int rank = 0;
-        for(auto &it: mp)
-            mp[it.first] = ++rank;
-        for(int i = 0; i < arr.size(); i++)
-            arr[i] = mp[arr[i]];
-        return arr;
+        map<int,int> m;
+        vector<int> ans;
+        int cnt=1;
+        for(auto i : arr)
+        {
+            m[i]++;
+        }
+        for(auto i : m)
+        {
+            m[i.first] = cnt;
+            cnt++;
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            ans.push_back(m[arr[i]]);
+        }
+        return ans;
     }
 };
