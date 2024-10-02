cb61df673e0d9aa982491436c53aaeb10192ff39
Siddhesh Kumar <siddheshkr21@gmail.com>
2024-10-02 14:41:41 +0530

    Time: 158 ms (5.34%) | Memory: 50.5 MB (13.06%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-        std::unordered_map<int, int> valueToRank;  // Map to store value-to-rank mapping
-        std::vector<int> sortedUniqueNumbers = arr; 
-        
-        // Remove duplicates and sort
-        std::sort(sortedUniqueNumbers.begin(), sortedUniqueNumbers.end());
-        sortedUniqueNumbers.erase(std::unique(sortedUniqueNumbers.begin(), sortedUniqueNumbers.end()), sortedUniqueNumbers.end());
-        
-        // Assign ranks to sorted unique elements
-        for (int i = 0; i < sortedUniqueNumbers.size(); i++) {
-            valueToRank[sortedUniqueNumbers[i]] = i + 1;
-        }
-
-        // Replace each element in the original array with its rank
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = valueToRank[arr[i]];
-        }
-
-        return arr;  // Return the updated array
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        map <int, int> mp;
+        set <int> s;
+        for(int i=0;i<arr.size();i++)
+            s.insert(arr[i]);          // making set that contains unique element in sorted order
+        int rank = 1;
+        for(auto &i : s)
+            mp[i] = rank,++rank;       // assinging rank to unique elements 
+        for(int i = 0; i < arr.size(); i++)
+            arr[i] = mp[arr[i]];
+        return arr;
     }
 };
