bd58af73a827b01dc677c41c44a3aed02b5fc124
Siddhesh Kumar <siddheshkr21@gmail.com>
2024-10-02 14:41:56 +0530

    Time: 101 ms (29.99%) | Memory: 43.6 MB (47.33%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         map <int, int> mp;
-        set <int> s;
-        for(int i=0;i<arr.size();i++)
-            s.insert(arr[i]);          // making set that contains unique element in sorted order
-        int rank = 1;
-        for(auto &i : s)
-            mp[i] = rank,++rank;       // assinging rank to unique elements 
+        vector <int> temp = arr;
+        int rank = 1; 
+        // sorting the array
+        sort(temp.begin(),temp.end()); 
+        // deleting repeated element
+        vector<int>::iterator it;
+        it = unique(temp.begin(), temp.end());
+        temp.resize(distance(temp.begin(), it));
+        //assinging rank
+        for(int i=0;i<temp.size();i++)
+            mp[temp[i]] = rank,++rank;
         for(int i = 0; i < arr.size(); i++)
             arr[i] = mp[arr[i]];
         return arr;
